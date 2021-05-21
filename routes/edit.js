be267dd0650cfb85d55228e5b118fa62150912d8
Ibrahim Gamal <hemagamal6@gmail.com>
2021-05-21 18:14:08 +0200

    islam

--- routes/edit.js
+++ routes/edit.js
@@ -5,8 +5,8 @@ const connection = require('../model/database')
 router.post("/admins", bodyParser.json(), function (req, res) {
     console.log(req.body)
     var admin = req.body.admin_code
-    var name = req.body.name
-    var email = req.body.email
+    var name = req.body.Name
+    var email = req.body.Email
     var phone = req.body.phone
     var sql = "UPDATE `admin` SET `Name`='" + name + "',`Email`='" + email + "',`phone`='" + phone + "' WHERE `admin_code`='" + admin + "';"
     connection.query(sql, function (err, result) {
@@ -20,8 +20,8 @@ router.post("/admins", bodyParser.json(), function (req, res) {
 });
 router.post("/users", bodyParser.json(), function (req, res) {
     var user = req.body.User_code
-    var name = req.body.name
-    var email = req.body.email
+    var name = req.body.Name
+    var email = req.body.Email
     var phone = req.body.phone
     var addr = req.body.address
     console.log(req.body)
