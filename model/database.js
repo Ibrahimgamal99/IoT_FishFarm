8a91044e5c4e7a6759394e3c57203d57d4317dbb
Ibrahim Gamal <hemagamal6@gmail.com>
2021-05-29 19:48:13 +0200

    change db host

--- model/database.js
+++ model/database.js
@@ -1,11 +1,12 @@
 var mysql = require('mysql');
 //Connect to Database
 var connection = mysql.createConnection({
-    host: 'sql4.freemysqlhosting.net',
-    user: 'sql4413446',
-    password: 'EbySIKnyB3',
-    database: 'sql4413446'
+    host: 'freedb.tech',
+    user: 'freedbtech_fishfarm',
+    password: 'iot1234567',
+    database: 'freedbtech_fishfarm'
 });
+
 
 connection.connect(function (err) {//check connection to database
     if (err) throw err;
@@ -14,8 +15,8 @@ connection.connect(function (err) {//check connection to database
 module.exports = connection;
 
 // var connection = mysql.createConnection({
-//     host: 'freedb.tech',
-//     user: 'freedbtech_fishfarm',
-//     password: 'iot1234567',
-//     database: 'freedbtech_fishfarm'
+//     host: 'sql4.freemysqlhosting.net',
+//     user: 'sql4413446',
+//     password: 'EbySIKnyB3',
+//     database: 'sql4413446'
 // });
