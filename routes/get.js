6dc198936da6cd720350a457b65891ec9f485cf2
Ibrahim Gamal <hemagamal6@gmail.com>
2021-07-08 13:49:53 +0200

    all users

--- routes/get.js
+++ routes/get.js
@@ -14,13 +14,21 @@ router.get("/users", bodyParser.json(), function (req, res) {
 });
 router.get("/admins", bodyParser.json(), function (req, res) {
     connection.query("SELECT * FROM `admin` ", function (err, result) {
-        console.log(result)
         res.status(200).json({
             result: result
         })
 
     })
 });
+
+router.get("/Allusers", bodyParser.json(), function (req, res) {
+    connection.query("SELECT * FROM `user` ", function (err, result) {
+        res.status(200).json({
+            result: result
+        })
+    })
+});
+
 router.get("/home/:name", bodyParser.json(), function (req, res) {
     var id = req.params.name
     connection.query("SELECT * FROM `Fishbowl` where `Farm_name`=?", id, function (err, result) {
