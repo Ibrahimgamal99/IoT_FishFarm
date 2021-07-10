af4da39bd4eb7dbfa03ded56a72c5df492043571
Ibrahim Gamal <hemagamal6@gmail.com>
2021-07-10 15:28:17 +0200

    update db

--- model/database.js
+++ model/database.js
@@ -1,12 +1,24 @@
 var mysql = require('mysql');
 //Connect to Database
-var connection = mysql.createConnection({
-    host: 'freedb.tech',
-    user: 'freedbtech_fishfarm',
-    password: 'iot1234567',
-    database: 'freedbtech_fishfarm'
-});
+// var connection = mysql.createConnection({
+//     host: 'freedb.tech',
+//     user: 'freedbtech_fishfarm',
+//     password: 'iot1234567',
+//     database: 'freedbtech_fishfarm'
+// });
 
+//Connect to Database
+try {
+    var connection = mysql.createConnection({
+        host: 'remotemysql.com',
+        user: '9jX1gUkrTN',
+        password: 'G85Cv3rSpj',
+        database: '9jX1gUkrTN'
+    });
+
+} catch (error) {
+    console.log(error)
+}
 
 connection.connect(function (err) {//check connection to database
     if (err) throw err;
--- routes/get.js
+++ routes/get.js
@@ -3,15 +3,7 @@ const bodyParser = require('body-parser');
 const e = require('cors');
 const connection = require('../model/database')
 const { end } = require('../model/database');
-router.get("/users", bodyParser.json(), function (req, res) {
-    connection.query("SELECT * FROM `user` ", function (err, result) {
-        console.log(result)
-        res.status(200).json({
-            result: result
-        })
 
-    })
-});
 router.get("/admins", bodyParser.json(), function (req, res) {
     connection.query("SELECT * FROM `admin` ", function (err, result) {
         res.status(200).json({
@@ -29,10 +21,9 @@ router.get("/Allusers", bodyParser.json(), function (req, res) {
     })
 });
 
-router.get("/home/:name", bodyParser.json(), function (req, res) {
+router.get("/userhome/:name", bodyParser.json(), function (req, res) {
     var id = req.params.name
-    connection.query("SELECT * FROM `Fishbowl` where `Farm_name`=?", id, function (err, result) {
-        console.log(result)
+    connection.query("SELECT * FROM `Fishbowl` where `Farm_name`='" + id + "' and `date` > now() - INTERVAL 7 day", id, function (err, result) {
         res.status(200).json({
             result: result
         })
@@ -40,5 +31,16 @@ router.get("/home/:name", bodyParser.json(), function (req, res) {
     })
 });
 
+router.get("/adminhome", bodyParser.json(), function (req, res) {
+    connection.query("SELECT * FROM `user` ", function (err, user) {
+        connection.query("SELECT * FROM `Farm` ", function (err, farm) {
+            res.status(200).json({
+                "num_users": user.length,
+                "num_Frams": farm.length,
+                "Farm": farm
+            })
+        })
+    })
+});
 
 module.exports = router;
