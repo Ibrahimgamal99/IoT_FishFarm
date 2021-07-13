a377fb40e99475c4d6961f5eaf1e8987b6bec46d
Ibrahim Gamal <hemagamal6@gmail.com>
2021-07-13 17:49:05 +0200

    temp

--- socket/live.js
+++ socket/live.js
@@ -25,7 +25,7 @@ module.exports = (io) => {
             io.in(User_code + farmname).emit('data', ({ farmname, tempc, ph }))
             console.log(User_code, farmname, tempc, ph)
             id = User_code
-            if (tempc < 15) {
+            if (tempc < 25) {
                 console.log("low")
                 io.in(User_code + farmname).emit('heater', 1)
             } else {
